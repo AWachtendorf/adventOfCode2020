c3ab103839da0e9fb51f8120652654c6635a0c1c
loslem <andreas.wachtendorf@gmx.de>
2020-12-20 20:21:07 +0100

    Constant refactorings to include go.mod imports

--- adv12/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/loslem/adventOfCode2020/adv12
-
-go 1.15
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	. "github.com/loslem/adventOfCode2020/adv1"
- 	. "github.com/loslem/adventOfCode2020/adv2"
+	. "github.com/loslem/adventOfCode2020/adv12/program/puzzleInput"
+	. "github.com/loslem/adventOfCode2020/adv12/program/solve"
+	. "github.com/loslem/adventOfCode2020/adv2"
 	. "github.com/loslem/adventOfCode2020/adv9/Puzzle"
 	. "github.com/loslem/adventOfCode2020/adv9/Puzzle1"
 	"sort"
@@ -32,5 +35,25 @@ func main(){
 	sort.Ints(j)
 
 	println(j[0] + j[len(j)-1])
+
+
+	cc := &CommandsList{}
+
+	cc.PrepareInput(PuzzleInputAdv12)
+	s := &Ship{}
+	w := &Waypoint{
+		North:  1,
+		East:   10,
+		South:  0,
+		West:   0,
+		Degree: 0,
+	}
+
+	for _, j := range cc.Com {
+		s.ReadCommands(w,j.Direction, j.Amount)
+		println(fmt.Sprintf("SHIP: N: %v, E: %v, S: %v, W: %v", s.North, s.East, s.South, s.West))
+		println(fmt.Sprintf("WAYPOINT: N: %v, E: %v, S: %v, W: %v", w.North, w.East, w.South, w.West))
+	}
+
 
 }
